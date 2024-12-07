488462a9cb8fecfae55e87eaceddc3900e93cd8c
RaihanNagata <Raihan.223040054@mail.unpas.ac.id>
2024-12-07 18:36:08 +0700

    menyelesaikan halaman login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    // isi di sini file nya
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
